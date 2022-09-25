9cd2812d1a05925cd9e3b7958d1497e8e6f6c629
jpmoormann <jeremy.paul.moormann@gmail.com>
2022-09-24 22:56:10 -0400

    retain root slash while trimming

--- routy.php
+++ routy.php
@@ -19,7 +19,7 @@ class Request
   {
     $this->method = $_SERVER['REQUEST_METHOD'];
     $upts = explode('?', $_SERVER['REQUEST_URI']);
-    $this->uri = str_replace($base, '', rtrim($upts[0], '/'));
+    $this->uri = $upts[0] != '/' ? str_replace($base, '', rtrim($upts[0], '/')) : '/';
     if (@$upts[1]) parse_str($upts[1], $this->query);
     $this->headers = (object)getallheaders();
   }

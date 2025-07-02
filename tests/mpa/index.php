c09c9f0fa15bcdacccdf309be311360e00889098
ginger-tek <moormann.jeremy.tech@gmail.com>
2025-07-01 21:37:09 -0400

    update tests

--- tests/mpa/index.php
+++ tests/mpa/index.php
@@ -6,13 +6,21 @@ use GingerTek\Routy;
 
 $app = new Routy(['layout' => 'views/_layout.php']);
 
+$app->setCtx('db', new PDO('sqlite:test.db', null, null, [
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+]));
+
 $app->get('/', fn($app) => $app->render('home'));
 
 $app->get('/ajax', fn($app) => $app->render('ajax'));
 
 $app->group('/api', function (Routy $app) {
   $app->get('/ajax', function (Routy $app) {
-    $app->sendJson(['msg' => 'Hello, world!']);
+    $db = $app->getCtx('db');
+    $stmt = $db->prepare("select 'Hello from SQLite!' as text");
+    $stmt->execute();
+    $app->sendJson(['msg' => $stmt->fetch()->text]);
   });
 
   $app->status(404)->sendJson(['error' => 'API not found']);
